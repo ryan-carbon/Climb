68f47f430f25ecff826d15eb45aabc180fac3f59
Ryan Sparks <ryan@carbonfinance.co.uk>
2023-01-13 17:25:44 +0000

    try connect to db attempt 10

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -2,14 +2,11 @@ import mysql from 'mysql2/promise'
 
 export const handler = async () => {
 	const connection = await mysql.createConnection(process.env.DATABASE_URL);
-	const [rows, fields] = await connection.query('SELECT 1');
+	const [rows] = await connection.query('SELECT * FROM Leaderboard;');
 	connection.end();
 	
 	return {
 		statusCode: 200,
-		body: JSON.stringify({
-			rows: rows,
-			fields: fields
-		})
+		body: JSON.stringify(rows)
 	}
 }
