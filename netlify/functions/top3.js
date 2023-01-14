056db1ca6039418fce099c70e6c25035fc4b4186
Ryan Sparks <ryan@carbonfinance.co.uk>
2023-01-14 21:35:53 +0000

    a

---
+++ netlify/functions/top3.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise'
+
+export const handler = async () => {
+	const connection = await mysql.createConnection(process.env.DATABASE_URL);	
+	const query = ['Orange', 'Yellow', 'Purple', 'Red', 'Pink', 'Black', 'Blue', 'White', 'Green'].map(circuit => `(SELECT Username, Circuit, Score FROM Leaderboard WHERE Circuit = "${circuit}" ORDER BY Score DESC, Username LIMIT 3)`).join(' UNION ALL ');
+	const [rows] = await connection.query(query);
+	connection.end();
+
+	let returnValue = {};
+	rows.forEach(row => returnValue[row.Colour] = (returnValue[row.Colour] || []).concat({ Username: row.Username, Score: row.Score }));
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify(returnValue)
+	}
+}
