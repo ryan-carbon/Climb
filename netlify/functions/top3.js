f8b4e3a94a90acd2f9fbb26edf0c3217c8ba5056
Ryan Sparks <ryan@carbonfinance.co.uk>
2023-01-14 00:05:43 +0000

    attempt to fix top3 function

--- netlify/functions/top3.js
+++ netlify/functions/top3.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise'
 
 export const handler = async () => {
 	const connection = await mysql.createConnection(process.env.DATABASE_URL);	
-	const query = ['Orange', 'Yellow', 'Purple', 'Red', 'Pink', 'Black', 'Blue', 'White', 'Green'].map(colour => `(SELECT Username, Colour, Score FROM Leaderboard WHERE Colour = ${colour} ORDER BY Score DESC, Username LIMIT 3)`).join(' union all ');
+	const query = ['Orange', 'Yellow', 'Purple', 'Red', 'Pink', 'Black', 'Blue', 'White', 'Green'].map(colour => `(SELECT Username, Colour, Score FROM Leaderboard WHERE Colour = "${colour}" ORDER BY Score DESC, Username LIMIT 3)`).join(' union all ');
 	const [rows] = await connection.query(query);
 	connection.end();
 	
