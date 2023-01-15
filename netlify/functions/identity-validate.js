992bf6063c4503958de90260dbac04ff5d724982
Ryan Sparks <ryan@carbonfinance.co.uk>
2023-01-15 12:59:50 +0000

    a

---
+++ netlify/functions/identity-validate.js
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch';
+
+exports.handler = async (event, context) => {
+  const { identity, user } = context.clientContext;
+  const usersUrl = `${identity.url}/admin/users?filter=${encodeURIComponent(user.name)}`;
+  const data = await fetch(usersUrl, {
+    method: 'GET',
+    headers: { Authorization: 'Bearer ' + identity.token },
+  }).then(res => res.json())
+
+  return {
+    statusCode: data.users.length > 0 ? 400 : 204
+  };
+};
