2075ee7de4c92cf084cd30352339f2f3698c3541
Ryan Sparks <ryan@carbonfinance.co.uk>
2023-01-15 16:00:28 +0000

    test

--- netlify/functions/validate.js
+++ netlify/functions/validate.js
@@ -1,7 +1,25 @@
-const fetch = require('node-fetch');
+const fetch = require('node-fetch')
 
-export const handler = async () => {
-	return {
-		statusCode: 200
-	};
-};
+const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
+
+exports.handler = async (event, context) => {
+  let response
+  try {
+    response = await fetch(API_ENDPOINT)
+    // handle response
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response
+    })
+  }
+}
